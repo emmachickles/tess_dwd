cdd5a1d9b2f1cc9e0c434ce471842ad674876171
Emma Chickles <echickle@wellesley.edu>
2022-06-28 08:53:56 -0400

    test

--- run_bls.py
+++ run_bls.py
@@ -1,3 +1,7 @@
+# with open('/home/submit/echickle/foo.txt', 'w') as f:
+#     f.write('help')
+
+
 output_dir = '/scratch/echickle/bls/'
 img_dir = output_dir + 'imgs/'
 data_dir = '/scratch/data/tess/lcur/spoc/raws/'
@@ -26,32 +30,49 @@ import astropy.units as u
 import lc 
 import pandas as pd 
 
+# with open('/home/submit/echickle/foo.txt', 'a') as f:
+#     f.write('ow')
+
+
+
+# lcfile = '/scratch/submit/tess/data/tesscurl_sector_5_lc/tess2018319095959-s0005-0000000471013547-0125-s_lc.fits'
+# hdul = fits.open(lcfile)
+# t = hdul[1].data['TIME']
+# y = hdul[1].data['PDCSAP_FLUX']
+# dy = np.ones(y.shape)
+
+# >> pmin: minimum period (minutes)
+# >> pmax: maximum period for dutycycles  (days)
+# >> qmin: minimum transit duraction (t_trans / period)
+# t, y, dy, period, bls_power_best = BLS(t,y,dy,pmin=3,pmax=True,qmin=2e-2,qmax=0.3,remove=True)
+# with open('/home/submit/echickle/foo.txt', 'w') as f:
+#     f.write('Period '+str(period)+'\nPower '+str(bls_power_best))
 # >> load white dwarf catalog
-wd_cat  = pd.read_csv('/home/echickle/work/tess_dwd/WDs.txt', header=None, sep='\s+')
+# wd_cat  = pd.read_csv('/home/echickle/work/tess_dwd/WDs.txt', header=None, sep='\s+')
 
 # >> WDs observed in S30 short cadence
 # s30 = np.loadtxt('/home/echickle/work/tess_dwd/all_targets_S030_v1.txt')
 # n = len(np.intersect1d(s30[:,0], wd_cat[0]))
 
 # >> load flux and time for white dwarfs in S30 (from TESS_Photometry.py)
-flux = np.load('/scratch/echickle/dwd/lc.npy').T
-time = np.load('/scratch/echickle/dwd/ts.npy') 
-ticid = np.load('/scratch/echickle/dwd/id.npy').astype('int')
+# flux = np.load('/scratch/echickle/dwd/lc.npy').T
+# time = np.load('/scratch/echickle/dwd/ts.npy') 
+# ticid = np.load('/scratch/echickle/dwd/id.npy').astype('int')
 
 # >> compute BLS
-for i in range(len(flux)):
-    y = flux[i]
-    inds = np.nonzero(~np.isnan(y))
-    t, y = time[inds], y[inds]
-    dy = np.ones(y.shape)
-    # default: pmin=3,pmax=True,qmin=2e-2,qmax=0.12
-    t, y, dy, period, bls_power_best = BLS(t,y,dy,pmin=3,pmax=True,qmin=2e-2,qmax=0.3,remove=True)
+# for i in range(len(flux)):
+#     y = flux[i]
+#     inds = np.nonzero(~np.isnan(y))
+#     t, y = time[inds], y[inds]
+#     dy = np.ones(y.shape)
+#     # default: pmin=3,pmax=True,qmin=2e-2,qmax=0.12
+#     t, y, dy, period, bls_power_best = BLS(t,y,dy,pmin=3,pmax=True,qmin=2e-2,qmax=0.3,remove=True)
 
-    # >> compute phase curve
-    prefix = str(bls_power_best)+'-'+str(round(period,2))+'-TIC%016d'%ticid[i]+'_'
-    lc.make_phase_curve(t, y, period, output_dir=img_dir, prefix=prefix)
+#     # >> compute phase curve
+#     prefix = str(bls_power_best)+'-'+str(round(period,2))+'-TIC%016d'%ticid[i]+'_'
+#     lc.make_phase_curve(t, y, period, output_dir=img_dir, prefix=prefix)
 
-pdb.set_trace()
+# pdb.set_trace()
 
 # fnames = []
 # for i in os.listdir(data_dir):
