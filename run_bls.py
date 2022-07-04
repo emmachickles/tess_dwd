e67d6c7020df157f4c9baa9e838914fe7c10a579
emmachickles <echickle@wellesley.edu>
2022-07-04 12:30:34 -0400

    bls test on 49 min

--- run_bls.py
+++ run_bls.py
@@ -27,24 +27,73 @@ from astropy.timeseries import TimeSeries
 from astropy.time import Time
 from astropy.coordinates import SkyCoord
 import astropy.units as u
-import lc 
+# import lc 
 import pandas as pd 
 
 # with open('/home/submit/echickle/foo.txt', 'a') as f:
 #     f.write('ow')
 
 
-
 # lcfile = '/scratch/submit/tess/data/tesscurl_sector_5_lc/tess2018319095959-s0005-0000000471013547-0125-s_lc.fits'
-# hdul = fits.open(lcfile)
-# t = hdul[1].data['TIME']
-# y = hdul[1].data['PDCSAP_FLUX']
-# dy = np.ones(y.shape)
+
+lcfile = '/scratch/echickle/tmp/tess2021204101404-s0041-0000001201247611-0212-s_lc.fits'
+hdul = fits.open(lcfile)
+t = hdul[1].data['TIME']
+y = hdul[1].data['PDCSAP_FLUX']
+
+# >> remove quality flags
+inds = np.nonzero(hdul[1].data['QUALITY'])
+t = np.delete(t, inds)
+y = np.delete(y, inds)
+
+inds = np.nonzero(~np.isnan(y))
+t, y = t[inds], y[inds]
+dy = np.ones(y.shape)
+plt.figure()
+plt.plot(t, y, '.k', ms=1)
+plt.xlabel('TIME')
+plt.ylabel('PDCSAP_FLUX')
+plt.title('TIC 1201247611, S41-20s')
+plt.savefig('/scratch/echickle/tmp/TIC1201247611_lc.png')
 
 # >> pmin: minimum period (minutes)
-# >> pmax: maximum period for dutycycles  (days)
+# >> pmax: maximum period (days)
 # >> qmin: minimum transit duraction (t_trans / period)
-# t, y, dy, period, bls_power_best = BLS(t,y,dy,pmin=3,pmax=True,qmin=2e-2,qmax=0.3,remove=True)
+pmin=3
+# pmax=True
+pmax=0.05 # = 72 minutes
+# qmin = 0.005
+# qmax = 0.01
+qmin=0.005
+qmax=0.05
+t, y, dy, period, bls_power_best, freqs, power = BLS(t,y,dy,pmin=pmin,pmax=pmax,
+                                       qmin=qmin,qmax=qmax,remove=True)
+
+
+plt.figure()
+plt.plot(freqs, power, '.k', ms=1)
+plt.xlim([0, 50])
+plt.xlabel('Frequency')
+plt.ylabel('BLS Power')
+plt.savefig('/scratch/echickle/tmp/TIC1201247611_bls.png')
+
+# >> compute phase curve
+ts = TimeSeries(time=Time(t, format='jd'), data={'flux': y})
+ts_folded = ts.fold(period=period*u.d) 
+
+sorted_inds = np.argsort(ts_folded['time'])
+folded_t = ts_folded['time'][sorted_inds]
+folded_y = ts_folded['flux'][sorted_inds]
+
+plt.figure()
+plt.plot(folded_t.value, folded_y.value, '.k', ms=1)
+plt.xlabel('Phase')
+plt.ylabel('PDCSAP_FLUX')
+plt.title('TIC 1201247611, pmin: '+str(qmin)+', pmax: '+str(pmax)+', qmin: '+str(qmin)+', qmax: '+str(qmax))
+fname = '/scratch/echickle/tmp/TIC1201247611_pc_qmin'+str(qmin)+'_qmax'+str(qmax)+'_period'+str(period)+'_sig'+str(bls_power_best)+'.png'
+plt.savefig(fname)
+print('Saved '+fname)
+
 # with open('/home/submit/echickle/foo.txt', 'w') as f:
 #     f.write('Period '+str(period)+'\nPower '+str(bls_power_best))
 # >> load white dwarf catalog
