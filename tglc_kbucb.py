616ddbe72c91cf9f544a2c14c12f92e0bd9caf36
emmachickles <echickle@wellesley.edu>
2023-04-10 10:07:54 -0400

    subimt 230410

--- tglc_kbucb.py
+++ tglc_kbucb.py
@@ -1,13 +1,8 @@
 import os
 from tglc.quick_lc import tglc_lc
 
-target = "TIC 2013499808"
-local_directory = "/data/submit/echickle/tess/echickle/"
-os.makedirs(local_directory, exist_ok=True)
-# tglc_lc(target=target, local_directory=local_directory, size=90,
-#         save_aper=False, limit_mag=20, get_all_lc=False,
-#         first_sector_only=False, sector=56,
-#         prior=None)
+local_directory = "/data/submit/echickle/kbucb/"
+target = "TIC 2040677137"
 tglc_lc(target=target, local_directory=local_directory, size=90,
         save_aper=False, limit_mag=20, get_all_lc=False,
         first_sector_only=False, sector=57,
