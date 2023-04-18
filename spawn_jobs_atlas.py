313e0c44b8de23c69b3e97b86b82122413a1b51d
echickle <echickle@eofe4.inband>
2023-04-18 09:38:17 -0400

    engaging 230418

--- spawn_jobs_atlas.py
+++ spawn_jobs_atlas.py
@@ -6,23 +6,23 @@ import multiprocessing
 from multiprocessing import Pool
 from run_bls_atlas import run_process
 
-data_dir = "/home/echickle/ATLAS_TEST/"
-# data_dir = "/pool001/echickle/ATLAS/"
+# data_dir = "/home/echickle/ATLAS_TEST/"
+data_dir = "/pool001/echickle/ATLAS/"
 p = [data_dir+f for f in os.listdir(data_dir)]
 
 N=int(sys.argv[1])
-sect=int(np.ceil(len(p)/16))
-print(quarter)
-if N<15:
-    p=p[N*sect:(N+1)*sect]
+N_sub = 500
+sub=int(np.ceil(len(p)/N_sub)) # 1393864 files, 500 sublists = 2780 files per job
+if N<N_sub-1:
+    p=p[N*sub:(N+1)*sub]
 else:
-    p=p[N*sect:]
+    p=p[N*sub:]
 
 # p = p[:9] # !!
 # p.insert(0, '/matchfiles/data2/ATLAS/3084580919978268672')
 
 if __name__ == '__main__':
-    N_p=2
+    N_p=16
     multiprocessing.set_start_method('spawn')
     pool = Pool(processes=N_p)
     result = pool.map(run_process, p) # gaiaid, sig, snr, wid, period, period_min, q, phi0
