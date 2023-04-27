fdbe743ecf5387d03750a109e3c69d5cdf0037e7
emmachickles <echickle@wellesley.edu>
2023-04-27 09:52:43 -0700

    hypernova.Caltech 230427

--- lc_utils.py
+++ lc_utils.py
@@ -327,7 +327,7 @@ def calc_sine_fit(t, y, period):
     
 def vet_plot(t, y, freqs, power, q=None, phi0=None, dy=None, output_dir=None, suffix='',
              objid=None, objid_type='TICID', bins=100, bls=True, save_npy=False, nearpeak=3000,
-             plot_threshold=10):
+             plot_threshold=50):
     '''Plot power spectrum and phase-folded light curve.
     * q : ratio of eclipse duration to period
     * phi0 : start of eclipse phase
@@ -368,7 +368,7 @@ def vet_plot(t, y, freqs, power, q=None, phi0=None, dy=None, output_dir=None, su
     folded_t, folded_y, folded_dy = bin_timeseries(t%period, y, bins, dy=dy)
     
     # print('Fold light curve done!')
-    if snr > plot_threshold: # == make plot ====================================
+    if sig > plot_threshold: # == make plot ====================================
 
         # -- initialize figure -------------------------------------------------
         fig = plt.figure(figsize=(8, 10), constrained_layout=True)
