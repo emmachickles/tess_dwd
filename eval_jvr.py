d1387268cb6da37a2f4d3469f8ef40e61dd9f1f5
emmachickles <echickle@wellesley.edu>
2023-06-22 13:54:18 -0400

    q

--- eval_jvr.py
+++ eval_jvr.py
@@ -5,12 +5,20 @@ import matplotlib.pyplot as plt
 from astropy.coordinates import SkyCoord
 import astropy.units as u 
 from astroquery.gaia import Gaia
+import lc_utils as lcu
 Gaia.ROW_LIMIT = 5
 Gaia.MAIN_GAIA_TABLE = "gaiadr3.gaia_source"
 
-out_dir = "/scratch/echickle/tess_vet1/"
+out_dir = "/scratch/echickle/s0056_eval/"
 data_dir = '/scratch/echickle/'
 os.makedirs(out_dir, exist_ok=True)
+lc_dir = '/scratch/data/tess/lcur/ffi/s0056-lc-ZTF/'
+qflag_dir = "/scratch/echickle/QLPqflags/"
+wd_tab= "/scratch/echickle/WDs.txt"
+wd_main = "/scratch/echickle/GaiaEDR3_WD_main.fits"
+rp_ext = "/scratch/echickle/GaiaEDR3_WD_RPM_ext.fits"
+sector = 56
+
 
 pmax = 0.15*1440
 cat = np.loadtxt("/scratch/echickle/ZTF_Eclipses.txt", usecols=(1,2,3))
@@ -25,6 +33,7 @@ gmag_wd = wd_cat[4].to_numpy().astype('float')
 # -- TESS signals --------------------------------------------------------------
 
 # >> all signals
+ticid_all = []
 ra_all = []
 dec_all = []
 power_all = []
@@ -58,6 +67,7 @@ nt_ucb = []
 dphi_ucb = []
 match_ucb = []
 per_ucb_match = []
+tic_ucb_match = []
 no_match_ucb = []
 
 # >> long period DWDs in the white dwarf catalog
@@ -73,23 +83,35 @@ nt_dwd = []
 dphi_dwd = []
 match_dwd = []
 per_dwd_match = []
+tic_dwd_match = []
 no_match_dwd = []
 
 # ticid, ra, dec, sig, snr, wid, period, period_min, q, phi0, dur, epo, rp, nt, dphi
-for sector in [56, 57, 58, 59, 60, 61, 62]:
+# # ticid, ra, dec, sig, snr, wid, period, period_min, q, phi0, epo, rp, nt, dphi
+for sector in [56]: # !!
     for cam in [1,2,3,4]:
-        for ccd in [1,2,3,4]:
-            f = '/scratch/echickle/s%04d/'%sector+'s%04d-gpu-res/'%sector+\
+        for ccd in [1,2,3,4]: 
+            f = '/scratch/echickle/s%04d-WD/'%sector+'s%04d-gpu-res/'%sector+\
             'GPU-{}-{}-{}.result'.format(sector, cam, ccd)
             cat = pd.read_csv(f, header=None, skiprows=1)
+            # ra_all.extend(cat[1])
+            # dec_all.extend(cat[2])
+            # power_all.extend(cat[3])
+            # snr_all.extend(cat[4])
+            # wid_all.extend(np.int64(cat[5]))
+            # per_all.extend(cat[6])
+            # nt_all.extend(np.int64(cat[13]))
+            # dphi_all.extend(cat[14])
+            ticid_all.extend(cat[0])
             ra_all.extend(cat[1])
             dec_all.extend(cat[2])
             power_all.extend(cat[3])
             snr_all.extend(cat[4])
             wid_all.extend(np.int64(cat[5]))
             per_all.extend(cat[6])
-            nt_all.extend(np.int64(cat[13]))
-            dphi_all.extend(cat[14])
+            nt_all.extend(np.int64(cat[12]))
+            dphi_all.extend(cat[13])
+
 
             cat_ticid = np.int64(cat[0])
             _, inds, inds1 = np.intersect1d(cat_ticid, ticid_tess, return_indices=True)
@@ -99,8 +121,8 @@ for sector in [56, 57, 58, 59, 60, 61, 62]:
             snr_tess.extend(cat[4][inds])
             wid_tess.extend(np.int64(cat[5])[inds])
             per_tess.extend(cat[6][inds])
-            nt_tess.extend(np.int64(cat[13])[inds])
-            dphi_tess.extend(cat[14][inds])
+            nt_tess.extend(np.int64(cat[12])[inds])
+            dphi_tess.extend(cat[13][inds])
 
             _, inds, inds1 = np.intersect1d(cat_ticid, ticid_ucb, return_indices=True)
             ra_ucb.extend(cat[1][inds])
@@ -110,8 +132,8 @@ for sector in [56, 57, 58, 59, 60, 61, 62]:
             wid_ucb.extend(np.int64(cat[5])[inds])
             per_bls = cat[6][inds].to_numpy()
             per_ucb.extend(per_bls)
-            nt_ucb.extend(np.int64(cat[13])[inds])
-            dphi_ucb.extend(cat[14][inds])
+            nt_ucb.extend(np.int64(cat[12])[inds])
+            dphi_ucb.extend(cat[13][inds])
 
             dt = 2/1440
             for i in range(len(per_bls)):
@@ -122,10 +144,10 @@ for sector in [56, 57, 58, 59, 60, 61, 62]:
                 (per_bls[i] > per_ucb_true[inds1][i]/2-dt \
                  and per_bls[i] < per_ucb_true[inds1][i]/2+dt):
                     match_ucb.append(True)
-                    per_ucb_match.append(per_ucb_true[inds1][i])
                 else:
                     match_ucb.append(False)
-                    per_ucb_match.append(per_ucb_true[inds1][i])
+                per_ucb_match.append(per_ucb_true[inds1][i])
+                tic_ucb_match.append(ticid_ucb[inds1][i])
 
             _, inds, inds1 = np.intersect1d(cat_ticid, ticid_dwd, return_indices=True)
             ra_dwd.extend(cat[1][inds])
@@ -135,8 +157,8 @@ for sector in [56, 57, 58, 59, 60, 61, 62]:
             wid_dwd.extend(np.int64(cat[5])[inds])
             per_bls = cat[6][inds].to_numpy()
             per_dwd.extend(per_bls)
-            nt_dwd.extend(np.int64(cat[13])[inds])
-            dphi_dwd.extend(cat[14][inds])
+            nt_dwd.extend(np.int64(cat[12])[inds])
+            dphi_dwd.extend(cat[13][inds])
 
             dt = 2/1440
             for i in range(len(per_bls)):
@@ -147,12 +169,13 @@ for sector in [56, 57, 58, 59, 60, 61, 62]:
                 (per_bls[i] > per_dwd_true[inds1][i]/2-dt \
                  and per_bls[i] < per_dwd_true[inds1][i]/2+dt):
                     match_dwd.append(True)
-                    per_dwd_match.append(per_dwd_true[inds1][i])
                 else:
                     # no_match_dwd.append(inds1[i])
                     match_dwd.append(False)
-                    per_dwd_match.append(per_dwd_true[inds1][i])
+                per_dwd_match.append(per_dwd_true[inds1][i])
+                tic_dwd_match.append(ticid_dwd[inds1][i])
 
+ticid_all = np.array(ticid_all)
 power_all = np.array(power_all)
 snr_all = np.array(snr_all)
 wid_all = np.array(wid_all)
@@ -168,6 +191,7 @@ nt_ucb = np.array(nt_ucb)
 dphi_ucb = np.array(dphi_ucb)
 match_ucb = np.array(match_ucb)
 per_ucb_match = np.array(per_ucb_match)
+tic_ucb_match = np.array(tic_ucb_match)
 
 pow_dwd = np.array(pow_dwd)
 snr_dwd = np.array(snr_dwd)
@@ -177,42 +201,47 @@ nt_dwd = np.array(nt_dwd)
 dphi_dwd = np.array(dphi_dwd)
 match_dwd = np.array(match_dwd)
 per_dwd_match = np.array(per_dwd_match)
+tic_dwd_match = np.array(tic_dwd_match)
 
 # -- initialize metric plots ---------------------------------------------------
 
 fig1, ax1 = plt.subplots()
 ax1.plot(power_all, snr_all, '.k', ms=1, alpha=0.5)
 ax1.plot(pow_tess, snr_tess, 'vr', label='TESS')
-ax1.plot(pow_ucb[match_ucb], snr_ucb[match_ucb], '>g', label='KBUCB')
-ax1.plot(pow_dwd[match_dwd], snr_dwd[match_dwd], 'oc', label='DWD')
 
 fig2, ax2 = plt.subplots()
 ax2.plot(power_all, wid_all, '.k', ms=1, alpha=0.5)
 ax2.plot(pow_tess, wid_tess, 'vr', label='TESS')
-ax2.plot(pow_ucb[match_ucb], wid_ucb[match_ucb], '>g', label='KBUCB')
-ax2.plot(pow_dwd[match_dwd], wid_dwd[match_dwd], 'oc', label='DWD')
 
 fig3, ax3 = plt.subplots()
 ax3.plot(nt_all, dphi_all, '.k', ms=1, alpha=0.5)
 ax3.plot(nt_tess, dphi_tess, 'vr', label='TESS')
-ax3.plot(nt_ucb[match_ucb], dphi_ucb[match_ucb], '>g', label='KBUCB')
-ax3.plot(nt_dwd[match_dwd], dphi_dwd[match_dwd], 'oc', label='DWD')
 
+if len(match_ucb) > 0:
+    ax1.plot(pow_ucb[match_ucb], snr_ucb[match_ucb], '>g', label='KBUCB')
+    ax2.plot(pow_ucb[match_ucb], wid_ucb[match_ucb], '>g', label='KBUCB')
+    ax3.plot(nt_ucb[match_ucb], dphi_ucb[match_ucb], '>g', label='KBUCB')
+if len(match_dwd) > 0:
+    ax1.plot(pow_dwd[match_dwd], snr_dwd[match_dwd], 'oc', label='DWD')
+    ax2.plot(pow_dwd[match_dwd], wid_dwd[match_dwd], 'oc', label='DWD')
+    ax3.plot(nt_dwd[match_dwd], dphi_dwd[match_dwd], 'oc', label='DWD')
 
 # -- recovery plots ------------------------------------------------------------
 
 
 print('KB UCB Recovery: '+str(np.count_nonzero(match_ucb))+' / '+str(len(per_ucb)))
+print('Recovered UCBs: '+','.join(tic_ucb_match.astype('str')))
 print('DWD Recovery: '+str(np.count_nonzero(match_dwd))+' / '+str(len(per_dwd)))
+print('Recovered DWDs: '+','.join(tic_dwd_match.astype('str')))
+
+co_wd = SkyCoord(ra=ra_wd, dec=dec_wd, unit=(u.degree, u.degree), frame='icrs')
 
 co = SkyCoord(ra=ra_all, dec=dec_all, unit=(u.degree, u.degree), frame='icrs')
-co_wd = SkyCoord(ra=ra_wd, dec=dec_wd, unit=(u.degree, u.degree), frame='icrs')
 idx, d2d, _ = co.match_to_catalog_sky(co_wd)
 good_idx_all = np.nonzero(d2d.to(u.arcsec).value < 2)
 gmag_all = gmag_wd[idx[good_idx_all]]
 
 co = SkyCoord(ra=ra_ucb, dec=dec_ucb, unit=(u.degree, u.degree), frame='icrs')
-co_wd = SkyCoord(ra=ra_wd, dec=dec_wd, unit=(u.degree, u.degree), frame='icrs')
 idx, d2d, _ = co.match_to_catalog_sky(co_wd)
 good_idx = np.nonzero(d2d.to(u.arcsec).value < 2)
 gmag = gmag_wd[idx[good_idx]]
@@ -221,9 +250,10 @@ fig4, ax4 = plt.subplots()
 ax4.plot(gmag_all, per_all[good_idx_all], '.k', ms=1,alpha=0.5)
 ax4.set_xlabel('Gaia Gmag')
 ax4.set_ylabel('Period [days]')
-ax4.plot(gmag[match_ucb], per_ucb[match_ucb], '>g', label='KBUCB-recovered')
-ax4.plot(gmag[~match_ucb], per_ucb_match[~match_ucb], 'Xr', label='KBUCB-unrecovered\nTrue period')
-ax4.plot(gmag[~match_ucb], per_ucb[~match_ucb], 'Xm', label='KBUCB-unrecovered\nBLS period')
+if len(match_ucb) > 0:
+    ax4.plot(gmag[match_ucb], per_ucb[match_ucb], '>g', label='KBUCB-recovered')
+    ax4.plot(gmag[~match_ucb], per_ucb_match[~match_ucb], 'Xr', label='KBUCB-unrecovered\nTrue period')
+    ax4.plot(gmag[~match_ucb], per_ucb[~match_ucb], 'Xm', label='KBUCB-unrecovered\nBLS period')
 ax4.legend()
 fig4.savefig(out_dir+'gmag_per_KBUCB.png', dpi=300)
 ax4.set_ylim([0, 0.4])
@@ -239,9 +269,10 @@ fig4, ax4 = plt.subplots()
 ax4.plot(gmag_all, per_all[good_idx_all], '.k', ms=1,alpha=0.5)
 ax4.set_xlabel('Gaia Gmag')
 ax4.set_ylabel('Period [days]')
-ax4.plot(gmag[match_dwd], per_dwd[match_dwd], 'oc', label='Long DWD-recovered')
-ax4.plot(gmag[~match_dwd], per_dwd_match[~match_dwd], 'Xr', label='Long DWD-unrecovered\nTrue period')
-ax4.plot(gmag[~match_dwd], per_dwd[~match_dwd], 'Xm', label='Long DWD-unrecovered\nBLS period')
+if len(match_dwd) > 0:
+    ax4.plot(gmag[match_dwd], per_dwd[match_dwd], 'oc', label='Long DWD-recovered')
+    ax4.plot(gmag[~match_dwd], per_dwd_match[~match_dwd], 'Xr', label='Long DWD-unrecovered\nTrue period')
+    ax4.plot(gmag[~match_dwd], per_dwd[~match_dwd], 'Xm', label='Long DWD-unrecovered\nBLS period')
 ax4.legend()
 fig4.savefig(out_dir+'gmag_per_DWD.png', dpi=300)
 ax4.set_ylim([0, 0.4])
@@ -309,23 +340,23 @@ ax1.plot(pow[match], snr[match], '<b', label='JVR')
 ax2.plot(pow[match], wid[match], '<b', label='JVR')
 ax3.plot(nt[match], dphi[match], '<b', label='JVR')
 
-gmag = []
-for i in range(len(ra)):
-    coord = SkyCoord(ra=ra[i], dec=dec[i],
-                     unit=(u.degree, u.degree), frame='icrs')
-    try:
-        width, height = u.Quantity(2, u.arcsec), u.Quantity(2, u.arcsec)
-        r = Gaia.query_object_async(coordinate=coord, width=width, height=height)
-        # j = Gaia.cone_search_async(co[i], radius=u.Quantity(3, u.arcsec))
-    except:
-        gmag.append(np.nan)
-    if len(r['phot_g_mean_mag']) > 0:
-        gmag.append(r['phot_g_mean_mag'][0])
-    else:
-        gmag.append(np.nan)
-gmag = np.array(gmag)
-np.savetxt(data_dir+'Gmag_JVR.txt', gmag)
-# gmag = np.loadtxt(data_dir + 'Gmag_JVR.txt')
+# gmag = []
+# for i in range(len(ra)):
+#     coord = SkyCoord(ra=ra[i], dec=dec[i],
+#                      unit=(u.degree, u.degree), frame='icrs')
+#     try:
+#         width, height = u.Quantity(2, u.arcsec), u.Quantity(2, u.arcsec)
+#         r = Gaia.query_object_async(coordinate=coord, width=width, height=height)
+#         # j = Gaia.cone_search_async(co[i], radius=u.Quantity(3, u.arcsec))
+#     except:
+#         gmag.append(np.nan)
+#     if len(r['phot_g_mean_mag']) > 0:
+#         gmag.append(r['phot_g_mean_mag'][0])
+#     else:
+#         gmag.append(np.nan)
+# gmag = np.array(gmag)
+# np.savetxt(data_dir+'Gmag_JVR.txt', gmag)
+gmag = np.loadtxt(data_dir + 'Gmag_JVR.txt')
 
 fig4, ax4 = plt.subplots()
 ax4.plot(gmag_all, per_all[good_idx_all], '.k', ms=1,alpha=0.5)
@@ -365,12 +396,12 @@ def get_stellar_density(ra, dec):
 
     return density
 
-stellar_density = []
-for i in range(len(ra)):
-    stellar_density.append(get_stellar_density(ra[i], dec[i]))
-stellar_density = np.array(stellar_density)
-np.savetxt(data_dir+'stellar_density_JVR.txt', stellar_density)
-# stellar_density = np.loadtxt(data_dir + 'stellar_density_JVR.txt')
+# stellar_density = []
+# for i in range(len(ra)):
+#     stellar_density.append(get_stellar_density(ra[i], dec[i]))
+# stellar_density = np.array(stellar_density)
+# np.savetxt(data_dir+'stellar_density_JVR.txt', stellar_density)
+stellar_density = np.loadtxt(data_dir + 'stellar_density_JVR.txt')
 
 
 fig4, ax4 = plt.subplots()
@@ -384,18 +415,65 @@ fig4.savefig(out_dir+'stellar_density_per_JVR.png', dpi=300)
 ax4.set_ylim([0, 0.4])
 fig4.savefig(out_dir+'stellar_density_per_JVR_zoom.png', dpi=300)
 
-# >> inspect BLS plots for unrecovered JVR 
-os.makedirs(out_dir+'unrecovered_JVR/', exist_ok=True)
-for f in fnames_jvr[~match]:
-    os.system('cp '+f+' '+out_dir+'unrecovered_JVR/')
-plot_dirs = np.array([fn.split('/')[4] for fn in fnames_jvr[~match]])
-labels, counts = np.unique(plot_dirs, return_counts=True)
-plt.figure(figsize=(10,6))
-x = range(len(labels))
-plt.bar(x, counts)
-_ = plt.xticks(x, labels, rotation='vertical')
-plt.tight_layout() 
-plt.savefig(out_dir+'unrecovered_JVR_bar.png')
+# # >> inspect BLS plots for unrecovered JVR 
+# os.makedirs(out_dir+'unrecovered_JVR/', exist_ok=True)
+# for f in fnames_jvr[~match]:
+#     os.system('cp '+f+' '+out_dir+'unrecovered_JVR/')
+# plot_dirs = np.array([fn.split('/')[4] for fn in fnames_jvr[~match]])
+# labels, counts = np.unique(plot_dirs, return_counts=True)
+# os.makedirs(out_dir+'unrecovered_JVR_bls/', exist_ok=True)
+
+# result = []
+# for period, f in zip(per_ztf[~match], fnames_jvr[~match]):
+#     new_f = f.split('/')[-1][:-4] + '-true_period.png'
+#     ticid = np.int64(f.split('_')[8][3:])
+#     cam = np.int64(f.split('_')[11])
+#     ccd = np.int64(f.split('_')[13])
+    
+
+#     ticid_ccd = np.load(lc_dir+'id-{}-{}.npy'.format(cam, ccd))
+#     ind = np.nonzero(ticid_ccd == ticid)[0]
+#     t = np.load(lc_dir+'ts-{}-{}.npy'.format(cam,ccd))
+#     y = np.load(lc_dir+'lc-{}-{}.npy'.format(cam,ccd))[ind]
+#     coord=np.load(lc_dir+'co-{}-{}.npy'.format(cam,ccd))[ind][0]
+#     cn = np.load(lc_dir+'cn-{}-{}.npy'.format(cam,ccd))
+
+#     t, y, cn = lcu.rm_qflag(t, y, cn, qflag_dir, sector, cam, ccd)
+#     t, y, flag = lcu.prep_lc(t, y)
+#     folded_t, folded_y, folded_dy = lcu.bin_timeseries(t%period, y, 100)
+#     fig, ax = plt.subplots()
+#     lcu.plot_phase_curve(ax, folded_t, folded_y, folded_dy, period=period)
+#     fig.savefig(out_dir+'unrecovered_JVR/'+new_f)
+#     print('Saved '+out_dir+'unrecovered_JVR/'+new_f)
+
+#     from Period_Finding import BLS
+#     dt = 5 # minutes
+#     pmin = period*1440. - dt # minutes
+#     pmax = period + dt/1440. # days
+#     dy = np.ones(y.shape)*0.1
+#     freqs_to_remove=lcu.rm_freq_tess()        
+#     t, y, dy, period, bls_power_best, freqs, power, q, phi0 = \
+#         BLS(t,y,dy,pmin=pmin,pmax=pmax,freqs_to_remove=freqs_to_remove)
+#     suffix = '_TIC%016d'%ticid+'_s%04d_'%sector+'cam_'+\
+#              str(cam)+'_ccd_'+str(ccd)+\
+#              '_ra_{}_dec_{}_'.format(coord[0], coord[1])
+#     res = lcu.vet_plot(t, y, freqs, power, q, phi0, output_dir=out_dir+'unrecovered_JVR_bls/',
+#                    objid=ticid, objid_type=None, suffix=suffix, 
+#                    ra=coord[0], dec=coord[1],
+#                    wd_main=wd_main, rp_ext=rp_ext, wd_tab=wd_tab)
+#     result.append([ticid, coord[0], coord[1]] + list(res))
+
+# np.savetxt(out_dir+'unrecovered_JVR_bls/GPU.result', np.array(result),
+#            fmt='%s,%10.5f,%10.5f,%10.5f,%10.5f,%i,%10.8f,%10.5f,%10.5f,%10.5f,%10.5f,%10.5f,%i,%10.5f',
+#            header='ticid, ra, dec, sig, snr, wid, period, period_min, q, phi0, epo, rp, nt, dphi')
+
+
+# plt.figure(figsize=(10,6))
+# x = range(len(labels))
+# plt.bar(x, counts)
+# _ = plt.xticks(x, labels, rotation='vertical')
+# plt.tight_layout() 
+# plt.savefig(out_dir+'unrecovered_JVR_bar.png')
 
 # -- save figures --------------------------------------------------------------
 
@@ -406,10 +484,10 @@ fig1.tight_layout()
 fig1.savefig(out_dir + 'pow_snr.png')
 print(out_dir + 'pow_snr.png')
 
-ax1.set_xlim([0, 200])
-ax1.set_ylim([0, max(snr_tess)])
-fig1.savefig(out_dir + 'pow_snr_zoom.png')
-print(out_dir + 'pow_snr_zoom.png')
+# ax1.set_xlim([0, 200])
+# ax1.set_ylim([0, max(snr_tess)])
+# fig1.savefig(out_dir + 'pow_snr_zoom.png')
+# print(out_dir + 'pow_snr_zoom.png')
 
 ax2.set_xlabel('Peak Significance = (peak - median)/MAD')
 ax2.set_ylabel('Peak width')
@@ -418,10 +496,10 @@ fig2.tight_layout()
 fig2.savefig(out_dir + 'pow_wid.png')
 print(out_dir + 'pow_wid.png')
 
-ax2.set_xlim([0, 200])
-ax2.set_ylim([0, max(wid_tess)])
-fig2.savefig(out_dir + 'pow_wid_zoom.png')
-print(out_dir + 'pow_wid_zoom.png')
+# ax2.set_xlim([0, 200])
+# ax2.set_ylim([0, max(wid_tess)])
+# fig2.savefig(out_dir + 'pow_wid_zoom.png')
+# print(out_dir + 'pow_wid_zoom.png')
 
 ax3.set_xlabel('Number of points in transit')
 ax3.set_ylabel('Dphi')
@@ -439,3 +517,43 @@ fig3.savefig(out_dir + 'nt_dphi_zoom.png')
 # plt.ylabel('Num objects in TESS S61')
 # plt.savefig(out_dir + 'pow_hist.png')
 # print(out_dir + 'pow_hist.png')
+
+inds = np.nonzero( (power_all > 16) * (snr_all > 5) * (nt_all>130) )
+np.savetxt('/scratch/data/tess/lcur/ffi/cvae_data/metric_cut.txt', np.int64(ticid_all[inds]))
+
+# >> Inspect periodic sources taht aren't DWDs, UCBs or JVRs
+save_dir = out_dir + 'new_periodic/'
+os.makedirs(save_dir, exist_ok=True)
+co_wd = SkyCoord(ra=ra_all, dec=dec_all, unit=(u.degree, u.degree), frame='icrs')
+wd_idx = []
+
+co_dwd = SkyCoord(ra=ra_dwd, dec=dec_dwd, unit=(u.degree, u.degree), frame='icrs')
+idx, d2d, _ = co.match_to_catalog_sky(co_wd)
+good_idx = np.nonzero(d2d.to(u.arcsec).value > 2)[0] # want unmatched
+if len(good_idx) > 0:
+    wd_idx.extend(idx[good_idx])
+
+co_ucb = SkyCoord(ra=ra_ucb, dec=dec_ucb, unit=(u.degree, u.degree), frame='icrs')
+idx, d2d, _ = co.match_to_catalog_sky(co_wd)
+good_idx = np.nonzero(d2d.to(u.arcsec).value > 2)[0] # want unmatched
+if len(good_idx) > 0:
+    wd_idx.extend(idx[good_idx])
+
+co_jvr = SkyCoord(ra=ra, dec=dec, unit=(u.degree, u.degree), frame='icrs')
+idx, d2d, _ = co.match_to_catalog_sky(co_wd)
+good_idx = np.nonzero(d2d.to(u.arcsec).value > 2)[0] # want unmatched
+if len(good_idx) > 0:
+    wd_idx.extend(idx[good_idx])
+
+wd_idx = np.array(wd_idx)
+
+
+for cam in [1,2,3,4]:
+    for ccd in [1,2,3,4]:
+        suffix = '-{}-{}'.format(cam,ccd)
+        plot_dir =  data_dir + 's%04d-WD/'%sector + 's%04d-bls'%sector+suffix+'/'
+        plot_fnames = np.array(os.listdir(plot_dir))
+        plot_ticid = np.array([int(plot.split('_')[8][3:]) for plot in plot_fnames])
+        for f, ticid in zip(plot_fnames, plot_ticid):
+            if ticid in ticid_all[wd_idx]: # if unmatched
+                os.system('cp '+plot_dir+f+' '+save_dir)
